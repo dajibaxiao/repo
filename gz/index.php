a533be01c49d57bed5a9df6b59e3c7ea3724db90
dajibaxiao <dajibaxiao@users.noreply.github.com>
2022-04-23 08:11:03 +0800

    saoma
    Saoma

---
+++ gz/index.php
@@ -0,0 +1,96 @@
+﻿<html data-kantu="1"><head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta charset="UTF-8">
+<title>到欧皇源码站or意见反馈</title>
+<script type="text/javascript">
+     // 对浏览器的UserAgent进行正则匹配，不含有微信独有标识的则为其他浏览器
+     var useragent = navigator.userAgent;
+     if (useragent.match(/MicroMessenger/i) != 'MicroMessenger') {
+         // 这里警告框会阻塞当前页面继续加载
+         alert('已禁止本次访问：您必须使用微信内置浏览器访问本页面！');
+         // 以下代码是用javascript强行关闭当前页面
+         var opened = window.open('about:blank', '_self');
+         opened.opener = null;
+         opened.close();
+     }
+ </script>
+<style type="text/css">
+body{
+margin:0px;
+padding:0px;
+font-family:Arial;
+border:0px;
+z-index:1;
+background-color: #f4f4f4;
+}	
+	.allnr{
+		width: 100%;
+		height: auto;
+		display: block;
+		max-width: 800px;
+		margin-top:20px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	
+	.gonggao{
+		width: auto;
+		height: auto;
+		margin-left: 20px;
+		margin-right: 20px;
+		background-color: #ffffff;
+		border-radius: 20px;
+		padding: 25px;
+		text-align:center;
+	}
+	
+	.ggtile{
+		font-size: 18px;
+		color: #000000;
+		font-weight: 900;
+		display: block;
+		margin-top: 0px;
+	}
+	
+	.ggnr{
+		color: #616264;
+		font-weight: normal;
+		font-size: 16px;
+		margin-top: 20px;
+		display: block;
+		color:blue
+	}
+	.back{
+		background-color: #000000;
+		color: white;
+		margin-top: 20px;
+		margin-left: 20px;
+		margin-right: 20px;
+		height: 50px;
+		line-height: 50px;
+		width: auto;
+		display: block;
+		border-radius: 10px;
+		text-decoration: none;
+		text-align: center;
+	}
+</style>
+</head>
+<body oncontextmenu="return false" onselectstart="return false">
+	<div class="allnr">
+		<div class="gonggao">
+		    			<!--<a class="ggtile">【欧皇源码ohbbs.cn】</a>-->
+			<a class="ggnr"><img src="../air.png" tppabs="https://img.airjpg.com/wp-content/uploads/2021/12/1638435754-9a3061ffad535cd.png" width="100%"></a>
+			<a class="ggnr"><b>支持苹果+安卓双系统</b></a>	
+<hr>
+<b><font color="#FF8000">【战力查询】【战区修改】【金牌特权】【空白名/重复名】
+</font></b>
+		<a class="ggnr"><b> 如需添加游戏或意见反馈可联系 </b></a>
+		<a class="ggnr"><b> 欧皇源码客服VX：xdoo10089 </b></a>
+		<a class="ggnr"><img src="./wx.jpg" tppabs="./wx.jpg" height="200" width="200"></a>
+		</div>
+		<a class="back" href="../index.html">返回主页</a>
+	</div>
+	
+</body>
+</html>
